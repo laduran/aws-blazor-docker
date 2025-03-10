7d59192b0e74e3ec573eff3733a89696fa4160f1
Louis <louis.duran@gmail.com>
2025-03-10 12:04:05 -0700

    Added some notes after meeting with Sergiy Zyuzko

--- IaC/main.tf
+++ IaC/main.tf
@@ -2,6 +2,8 @@ provider "aws" {
   region = "us-west-2"
 }
 
+
+## Do I even need this if I am using ECS to run the containers?
 resource "aws_instance" "web_server" {
   ami               = "ami-04a81a99f5ec58529"
   instance_type     = "t2.micro"
@@ -17,3 +19,19 @@ resource "aws_instance" "web_server" {
     Name = "web-server"
   }
 }
+
+# something to store the terraform state file?
+# S3 bucket for storing terraform state file
+# DynamoDB table for locking state file?
+
+# ECR?
+# Fargate cluster (also ECS cluster)
+# VPC
+# Subnet
+# Route Table
+# Load balancer?
+# RDS for database configured to PostgreSQL instace.
+# S3 bucket for storing static files
+
+# Route 53 settings for domain  <-- This doesn't need to be managed through Terraform
+
